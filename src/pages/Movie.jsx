62631ac6a97dbab75874990bf1eac468e0b1c6d7
thiago alves <thiago87gsa@gmail.com>
2023-12-16 12:54:30 -0300

    responsividade pagina de detalhes

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -30,40 +30,42 @@ const Movie = () => {
     getMovie(movieUrl);
   }, [id]);
 
-  const formatCurrency = (number) =>{
-    return number.toLocaleString('en-US',{
-      style: 'currency',
-      currency: 'USD',
-    })
-  }
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
 
   return (
     <div className="movie_page">
       {movie && (
         <>
-          <MovieCard movie={movie} showLink={false} />
+          <div className="img">
+            <MovieCard movie={movie} showLink={false} />
+          </div>
           <p className="tagline">{movie.tagline}</p>
           <div className="info">
             <h3>
-              <BsWallet2/> Orçamento:
+              <BsWallet2 /> Orçamento:
             </h3>
             <p>{formatCurrency(movie.budget)}</p>
           </div>
           <div className="info">
             <h3>
-              <BsGraphUp/> Receita:
+              <BsGraphUp /> Receita:
             </h3>
             <p>{formatCurrency(movie.revenue)}</p>
           </div>
           <div className="info">
             <h3>
-              <BsHourglassSplit/> Duração:
+              <BsHourglassSplit /> Duração:
             </h3>
             <p>{movie.runtime} minutos</p>
           </div>
           <div className="info description">
             <h3>
-              <BsFillFileEarmarkTextFill/> descrição:
+              <BsFillFileEarmarkTextFill /> descrição:
             </h3>
             <p>{movie.overview}</p>
           </div>
